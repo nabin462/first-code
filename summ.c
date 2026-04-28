2595e7e722312633e36b30151bcd7930252a5ceb
nabin462 <budhathokinabin700@gmail.com>
2026-04-28 16:43:28 +0545

    Add files via upload

---
+++ summ.c
@@ -0,0 +1,11 @@
+// write a program to sum of two number
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter the  two number\n ");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("sum=%d",sum);
+	return 0;
+}
