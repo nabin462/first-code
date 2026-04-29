9b879734a81144c1f436ea346166b6ea55b4e8f4
nabin462 <budhathokinabin700@gmail.com>
2026-04-29 13:34:07 +0545

    Add files via upload

---
+++ area.c
@@ -0,0 +1,13 @@
+//WAP to find the area of circle.
+#include<stdio.h>
+#define pi 3.14
+int main()
+{
+	int r,a;
+	printf("enter the radius of circle\n");
+	scanf("%d",&r);
+	a=pi*r*r;
+	printf("area=%d",a);
+	return 0;
+	
+}
