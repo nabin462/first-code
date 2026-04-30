708000fcaadacb88011c7b17d34cba979cdf3671
nabin462 <budhathokinabin700@gmail.com>
2026-04-30 10:06:01 +0545

    Add files via upload

---
+++ num positive .c
@@ -0,0 +1,18 @@
+//WAP to check the given number is positive or negative
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number\n");
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("The number is positive\n");
+		
+	}
+	else
+	{
+		printf("The number is negative\n");
+	}
+	return 0;
+}
