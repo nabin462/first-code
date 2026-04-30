d0842f2a615482ee50174b9d62c0b165ab58115e
nabin462 <budhathokinabin700@gmail.com>
2026-04-30 15:56:18 +0545

    Add files via upload

---
+++ num odd,even.c
@@ -0,0 +1,17 @@
+//WAP to  given number is odd or even
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number \n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("the number is even");
+	}
+	else
+	{
+		printf("the number is odd");
+	}
+	return 0;
+}
