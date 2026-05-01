afe4bb36d1d390ba99b8c5fbe96833c3bd373307
nabin462 <budhathokinabin700@gmail.com>
2026-05-01 08:43:56 +0545

    Add files via upload

---
+++ larger among two nm.c
@@ -0,0 +1,17 @@
+//WAP to larger among two number
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("enter two number\n");
+	scanf("%d %d",&x,&y);
+	if(x>y)
+	{
+		printf("x=%d is larger",x);
+	}
+	else
+	{
+		printf("y is larger");
+	}
+	return 0;
+}
