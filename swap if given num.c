1316e30882263d75885be1551655f86fcf4ea9ca
nabin462 <budhathokinabin700@gmail.com>
2026-05-02 16:17:42 +0545

    Add files via upload

---
+++ swap if given num.c
@@ -0,0 +1,13 @@
+//WAP to swap of given number
+#include<stdio.h>
+int main()
+{
+	int a,b,temp;
+	printf("enter two number\n");
+	scanf("a=%d b=%d",a,b);
+	temp=a;
+	a=b;
+	b=temp;
+	printf("a=%d b=%d",a,b);
+	return 0;
+}
